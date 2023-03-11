b266620bee7908b4b6b9cb42f57f62b8f2ca6ba5
Meu Nome <meu-email@meu-servidor.com>
2023-03-10 22:36:30 -0300

    Frutas

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Frutas preferidas");
+Console.WriteLine("Banana");
+Console.WriteLine("Morango");
+Console.WriteLine("Figo");
+Console.WriteLine("Pêssego");
+Console.WriteLine("Laranja");
